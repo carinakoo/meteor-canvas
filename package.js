fce057af6258109337b620459b2c5259c52d77d1
Carina Koo <carinakoo@gmail.com>
2016-03-28 21:44:36 -0700

    updating build for debian wheezy -- mupx default deploy setup

--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
   name: 'carinakoo:canvas',
-  version: '0.1.0',
+  version: '0.1.2',
   summary: "Wrapper for the NPM 'canvas' package",
   git: 'https://github.com/carinakoo/meteor-canvas.git',
   documentation: 'README.md'
 });
 
 Npm.depends({
-    canvas:'1.3.12'
+    // OSX custom font workaround requires canvas version 1.1.6: 
+    // https://github.com/Automattic/node-canvas/issues/548
+    canvas:'1.1.6'
 });
 
 Package.onUse(function(api) {
